acccbb028a781a15c18989f6042ba26435d684ce
JulianHG-UAN <julhernandez21@uan.edu.co>
2022-10-25 19:29:45 -0500

    Tarea UO9: Elaboracion inicial del controlador para POST de etiqueta

--- src/main/java/co/edu/uan/software/tasky/controllers/U09_post_etiquetas_controller.java
+++ src/main/java/co/edu/uan/software/tasky/controllers/U09_post_etiquetas_controller.java
@@ -1,5 +1,22 @@
 package co.edu.uan.software.tasky.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.uan.software.tasky.entities.U09_post_etiquetas_entity;
+
+@RestController
 public class U09_post_etiquetas_controller {
     
+    @PostMapping("/etiqueta")
+    public U09_post_etiquetas_entity etiqueta() {
+
+        return new U09_post_etiquetas_entity();
+    }
+
+
+
+
+
+
 }

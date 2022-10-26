120079392e396a4da1b3546f34e83887a63e7e03
JulianHG-UAN <julhernandez21@uan.edu.co>
2022-10-25 19:04:06 -0500

    Tarea UO9: Elaboracion de la entidad y el repositorio de las etiquetas

--- src/main/java/co/edu/uan/software/tasky/entities/U09_post_etiquetas_entity.java
+++ src/main/java/co/edu/uan/software/tasky/entities/U09_post_etiquetas_entity.java
@@ -1,5 +1,51 @@
 package co.edu.uan.software.tasky.entities;
 
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name = "etiqueta")
 public class U09_post_etiquetas_entity {
-    
+
+    @Id
+    @GeneratedValue
+    private UUID uid;
+    private String nombreEtiqueta;
+    private String color;
+
+    @Override
+    public String toString() {
+        return "{" +
+            " uid='" + getUid() + "'" +
+            ", nombreEtiqueta='" + getNombreEtiqueta() + "'" +
+            ", color='" + getColor() + "'" +
+            "}";
+    }
+
+    public UUID getUid() {
+        return this.uid;
+    }
+
+    public void setUid(UUID uid) {
+        this.uid = uid;
+    }
+
+    public String getNombreEtiqueta() {
+        return this.nombreEtiqueta;
+    }
+
+    public void setNombreEtiqueta(String nombreEtiqueta) {
+        this.nombreEtiqueta = nombreEtiqueta;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
 }
--- src/main/java/co/edu/uan/software/tasky/repositories/U09_post_etiquetas_repository.java
+++ src/main/java/co/edu/uan/software/tasky/repositories/U09_post_etiquetas_repository.java
@@ -1,5 +1,9 @@
 package co.edu.uan.software.tasky.repositories;
 
-public interface U09_post_etiquetas_repository {
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import co.edu.uan.software.tasky.entities.U09_post_etiquetas_entity;
+
+public interface U09_post_etiquetas_repository extends JpaRepository<U09_post_etiquetas_entity, UUID>{
     
 }

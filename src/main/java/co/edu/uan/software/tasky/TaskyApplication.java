4b3ca18bc28452c2299afc33e54a342edb57625e
wjfatuan <wilson.forero@uan.edu.co>
2022-11-20 10:19:28 -0500

    Adding CORS support

--- src/main/java/co/edu/uan/software/tasky/TaskyApplication.java
+++ src/main/java/co/edu/uan/software/tasky/TaskyApplication.java
@@ -2,6 +2,9 @@ package co.edu.uan.software.tasky;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Tasky application.
@@ -12,5 +15,15 @@ public class TaskyApplication {
     public static void main(String[] args) {
         SpringApplication.run(TaskyApplication.class, args);
     }
+
+    @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	}
 
 }
